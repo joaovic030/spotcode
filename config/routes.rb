16f9ccb3c9a07cac540ffddf6d0c6b91127d2ac9
João Victor Rocha <joao@joaovrocha.com>
2020-04-16 14:35:09 -0400

    Endpoints do discovery

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
 end
